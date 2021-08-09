afeea4cf076192c66e418886184ea4021a910866
Hafedh Amri <amri_hafedh@yahoo.com>
2021-08-09 19:27:13 +0200

    changement de PlantItem.js

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -4,15 +4,21 @@ import '../styles/PlantItem.css';
 
 function PlantItem({ name, cover, id, light, water }) {
 
+    function handleClick(plantName) {
+        alert(`✨ Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱 ✨`);
+    }
+
     return (
-        <li key={id} className='lmj-plant-item'>
+        <li key={id} className='lmj-plant-item' onClick={handleClick}>
             <img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
             {name}
             <div>
                 <CareScale careType='water' scaleValue={water} />
                 <CareScale careType='light' scaleValue={light} />
             </div>
+
         </li>
     )
 }
+
 export default PlantItem;
